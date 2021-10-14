5fe4533cf16221296b135529a87d1624a8b1e817
Jaime Muñoz <jaime.munozq@udea.edu.co>
2021-10-13 22:00:14 -0500

    agregar verificacion con token

--- server.js
+++ server.js
@@ -1,16 +1,32 @@
-import Express from 'express';
-import Cors from 'cors';
-import dotenv from 'dotenv';
-import { connectDB } from './db/db.js';
-import routesProducts from './views/productos/rutas.js';
-import routesUsers from './views/usuarios/rutas.js';
+import Express from "express";
+import Cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./db/db.js";
+import routesProducts from "./views/productos/rutas.js";
+import routesUsers from "./views/usuarios/rutas.js";
+import jwt from "express-jwt";
+import jwks from "jwks-rsa";
 
-dotenv.config({ path: './.env' });
+dotenv.config({ path: "./.env" });
 
 const app = Express();
 
 app.use(Express.json());
 app.use(Cors());
+
+var jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://deveapps.us.auth0.com/.well-known/jwks.json",
+  }),
+  audience: "api-autenticacion-deveapps",
+  issuer: "https://deveapps.us.auth0.com/",
+  algorithms: ["RS256"],
+});
+
+app.use(jwtCheck);
 app.use(routesProducts);
 app.use(routesUsers);
 
@@ -19,5 +35,4 @@ const main = () => {
     console.log(`escuchando puerto ${process.env.PORT}`);
   });
 };
- connectDB(main);
-
+connectDB(main);
